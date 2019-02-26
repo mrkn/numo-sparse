9baefc1534b4991c1dfa56ed88e6d45f19c0759e
Kenta Murata <mrkn@mrkn.jp>
2019-02-26 22:13:34 +0900

    Remove spec dir

--- spec/numo/sparse_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe Numo::Sparse do
-  it "has a version number" do
-    expect(Numo::Sparse::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end

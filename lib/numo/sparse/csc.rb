08aea18ce6f435c356f57538ca693e748b1b306e
David <davidgomez81982@gmail.com>
2019-07-22 14:10:17 +0900

    This is the to_narray function

--- lib/numo/sparse/csc.rb
+++ lib/numo/sparse/csc.rb
@@ -25,6 +25,14 @@ module Numo
         @data = []
       end
 
+      # Creates the sparse matrix in csc format
+      # @param narray [narray] the matrix that will be converted
+      # @return [array] the converted matrix in csc format
+      # @example
+      #   narray = Numo::DFloat[[1, 0, 4], [0, 0, 5], [2, 3, 6]]
+      #   csc = Numo::Sparse::CSC.new(naray)
+      #   csc.indices
+      #   # => [0, 2, 2, 0, 1, 2]
       private def make_csc(narray)
         row_limit, col_limit = shape[0], shape[1]
         curr_col, count = 0, 0
@@ -48,6 +56,25 @@ module Numo
         @data = narray.class[*data]
         @indices = Numo::Int32[*indices]
         @indptr = Numo::Int32[*indptr]
+      end
+
+      def to_narray()
+        narray = data.class.zeros(shape)
+        col, current, curr_ind = 0, 0, 0
+        curr_data, col_lim, curr_ptr = 0, 0, 1
+        while col < (indptr.size - 1)
+          col_lim = (indptr[curr_ptr] - indptr[curr_ptr-1])
+          while current < col_lim
+            narray[indices[curr_ind],col] = data[curr_data]
+            curr_ind += 1
+            curr_data += 1
+            current += 1
+          end
+          curr_ptr += 1
+          col += 1
+          current = 0
+        end
+        narray
       end
     end
   end

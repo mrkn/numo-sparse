82f473b0307760306f2f7d66d19fdabdba0ce328
David <davidgomez81982@gmail.com>
2019-07-22 14:22:43 +0900

    added the ability to make a csr sparse matrix

--- lib/numo/sparse/csr.rb
+++ lib/numo/sparse/csr.rb
@@ -23,6 +23,31 @@ module Numo
         @dtype = check_dtype(dtype)
         @data = []
       end
+
+        private def make_csr(narray)
+        row_limit, col_limit = shape[0], shape[1]
+        matrix, curr_row, count = narray, 0, 0
+        indices = []
+        indices_temp = [] #this is for the transpose
+        indptr = []
+        indptr[0] = 0
+        while curr_row < row_limit
+          curr_col = 0
+          while curr_col < col_limit
+            if matrix[curr_row,curr_col] != 0
+              count += 1
+              indices.push(curr_col)
+              indices_temp.push(curr_row)
+            end
+            curr_col += 1
+          end
+          indptr.push(count)
+          curr_row += 1
+        end
+        @indices = Numo::Int32[*indices]
+        @indices_temp = Numo::Int32[*indices_temp]
+        @indptr = Numo::Int32[*indptr]
+      end
     end
   end
 end
